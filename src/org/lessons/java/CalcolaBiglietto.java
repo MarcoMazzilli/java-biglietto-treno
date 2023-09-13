1870cf25ca6dc5d32d72ef45abd34337152354ba
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-13 17:07:46 +0200

    [Refactoring] improved the code

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -6,42 +6,47 @@ public class CalcolaBiglietto {
 	
 	public static void main(String[] args) {
 		
-		float kmPrice = 0.21F ;
-		
+		//Scanner open
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Inserisci i km. da percorrere");
 		float km = sc.nextFloat();
-		System.out.println(km);
 		
 		System.out.println("Inserisci la tua età");
 		int age = sc.nextInt();
-		System.out.println(age);
 		
 		sc.close();
+		//Scanner close
 		
+		// DATA
+		float kmPrice = 0.21F ;
 		float totalPrice = km * kmPrice;
-		System.out.println("Tot. price " + totalPrice);
+		
 		int discountJunior = 20;
 		int discountSenior = 40;
+		// DATA
 
 		
 		if(age <= 18) {
 			
-			float totalDiscount =  totalPrice * discountJunior / 100;
-			float discountedPrice = totalPrice - totalDiscount ;
+			float discountedPrice =  totalPrice * (1-(discountJunior/100f));
+			String formattedPrice = String.format("%.2f", discountedPrice);
 
-			System.out.println("discountedPrice " + discountedPrice);
+			System.out.println("formattedPrice " + formattedPrice);
 
-		}else if (age >= 65){
+		}
+		else if (age >= 65){
 
-			float totalDiscount =  totalPrice * discountSenior / 100;
-			float discountedPrice = totalPrice - totalDiscount ;
+			float discountedPrice =  totalPrice * (1-(discountSenior/100f));
+			String formattedPrice = String.format("%.2f", discountedPrice);
 
-			System.out.println("discountedPrice " + discountedPrice);
+			System.out.println("formattedPrice " + formattedPrice);
 				
 		}else {	
-			System.out.println(totalPrice);
+			
+			String formattedPrice = String.format("%.2f", totalPrice);
+			System.out.println(formattedPrice);
+			
 		}
 		
 	}
